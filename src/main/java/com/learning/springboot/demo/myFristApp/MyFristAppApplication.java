e398ac0bfd3d8ef07880e6a9589c4140ee1bc13c
Aditya Gaurav <adityagaurav15898@gmail.com>
2024-12-12 16:36:17 +0530

    init commit

---
+++ src/main/java/com/learning/springboot/demo/myFristApp/MyFristAppApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springboot.demo.myFristApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFristAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFristAppApplication.class, args);
+	}
+
+}
